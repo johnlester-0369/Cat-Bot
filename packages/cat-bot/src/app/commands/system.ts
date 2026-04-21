6a1777dcf8c8207c3e94239a20dc289ab232b2c7
johnlester-0369 <johnlester.0369@gmail.com>
2026-04-21 12:02:00 +0800

    refactor(commands): apply CommandConfig to system

--- packages/cat-bot/src/app/commands/system.ts
+++ packages/cat-bot/src/app/commands/system.ts
@@ -24,8 +24,9 @@ import { Role } from '@/engine/constants/role.constants.js';
 import { MessageStyle } from '@/engine/constants/message-style.constants.js';
 import { ButtonStyle } from '@/engine/constants/button-style.constants.js';
 import { hasNativeButtons } from '@/engine/utils/ui-capabilities.util.js';
+import type { CommandConfig } from '@/engine/types/module-config.types.js';
 
-export const config = {
+export const config: CommandConfig = {
   name: 'system',
   aliases: ['sysinfo'] as string[],
   version: '1.0.0',
